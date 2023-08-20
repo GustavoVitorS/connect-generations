6244166222e6e9bd087d0731b564488c150f0a32
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2023-08-20 17:28:21 -0300

    Quase la novamente

--- frontend/api.js
+++ frontend/api.js
@@ -1,41 +1,51 @@
-
+updateInicial();
 const input = document.querySelector("input");
 input.addEventListener("input", updateValue);
-
-function updateValue(e) {
-  let valor = e.target.value;
-  let apagar = document.getElementById("giriaContainer")
-  apagar.innerHTML = "";
-  fetch('http://localhost:8080/termos/listarFiltrado?termo=' + valor + "&size=10")
-  .then(response => response.json())
-  .then(data => {
-    dados = data;
-    console.log(data);
-    for (let index = 0; index < dados.content.length; index++) {
-      var tag = document.createElement("div");
-      var primeiroP = document.createElement("p");
-      var segundoP = document.createElement("p");
-      var text = document.createTextNode(dados.content[index].termo);
-      var exp = document.createTextNode(dados.content[index].significado);
-      segundoP.style.fontSize = "10px";
-      segundoP.style.fontStyle = "italic";
-      primeiroP.appendChild(text);
-      segundoP.appendChild(exp);
-      tag.appendChild(primeiroP);
-      tag.appendChild(segundoP);
-      var element = document.getElementById("giriaContainer");
-      element.appendChild(tag);
-    }
-  })
-  .catch(error => {
-    console.error('Erro ao obter dados da API:', error);
-  });
-}
 let dados;
 
-fetch('http://localhost:8080/termos/listar?size=10')
+
+function updateValue(e) {
+  
+  let valor = e.target.value;
+
+  if (valor == "") {
+    updateInicial();
+  } else {
+    let apagar = document.getElementById("giriaContainer")
+    apagar.innerHTML = "";
+    fetch('http://localhost:8080/termos/listarFiltrado?termo=' + valor + "&size=10")
+    .then(response => response.json())
+    .then(data => {
+      dados = data;
+      console.log(data);
+      for (let index = 0; index < dados.content.length; index++) {
+        var tag = document.createElement("div");
+        var primeiroP = document.createElement("p");
+        var segundoP = document.createElement("p");
+        var text = document.createTextNode(dados.content[index].termo);
+        var exp = document.createTextNode(dados.content[index].significado);
+        segundoP.style.fontSize = "10px";
+        segundoP.style.fontStyle = "italic";
+        primeiroP.appendChild(text);
+        segundoP.appendChild(exp);
+        tag.appendChild(primeiroP);
+        tag.appendChild(segundoP);
+        var element = document.getElementById("giriaContainer");
+        element.appendChild(tag);
+      }
+    })
+    .catch(error => {
+      console.error('Erro ao obter dados da API:', error);
+    });
+  } 
+}
+
+function updateInicial() {
+  fetch('http://localhost:8080/termos/listar?size=10')
   .then(response => response.json())
   .then(data => {
+    let apagar = document.getElementById("giriaContainer")
+    apagar.innerHTML = "";
     dados = data;
     console.log(data);
     for (let index = 0; index < dados.content.length; index++) {
@@ -54,9 +64,8 @@ fetch('http://localhost:8080/termos/listar?size=10')
       var element = document.getElementById("giriaContainer");
       element.appendChild(tag);
     }
-  })
+  })  
   .catch(error => {
     console.error('Erro ao obter dados da API:', error);
   });
-
-
+}
